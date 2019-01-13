10864c2119a461adc62475f55f6c3efd2f3a7fb0
Sahar Haddad <saharh09@gmail.com>
2019-01-13 18:43:17 +0200

    logging

--- packages/firebase_messaging/android/src/main/java/io/flutter/plugins/firebasemessaging/FlutterFirebaseMessagingService.java
+++ packages/firebase_messaging/android/src/main/java/io/flutter/plugins/firebasemessaging/FlutterFirebaseMessagingService.java
@@ -6,24 +6,34 @@ package io.flutter.plugins.firebasemessaging;
 
 import android.content.Intent;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FlutterFirebaseMessagingService extends FirebaseMessagingService {
 
-  public static final String ACTION_REMOTE_MESSAGE =
-      "io.flutter.plugins.firebasemessaging.NOTIFICATION";
-  public static final String EXTRA_REMOTE_MESSAGE = "notification";
+    public static final String ACTION_REMOTE_MESSAGE =
+            "io.flutter.plugins.firebasemessaging.NOTIFICATION";
+    public static final String EXTRA_REMOTE_MESSAGE = "notification";
 
-  /**
-   * Called when message is received.
-   *
-   * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
-   */
-  @Override
-  public void onMessageReceived(RemoteMessage remoteMessage) {
-    Intent intent = new Intent(ACTION_REMOTE_MESSAGE);
-    intent.putExtra(EXTRA_REMOTE_MESSAGE, remoteMessage);
-    LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-  }
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        String data = null, notification = null;
+        if (remoteMessage.getData() != null) {
+            data = remoteMessage.getData().toString();
+        }
+        if (remoteMessage.getNotification() != null) {
+            notification = "{ body: " + remoteMessage.getNotification().getBody() + ", title: " + remoteMessage.getNotification().getTitle() + " }";
+        }
+        Log.d("tag", "onMessageReceived: data: " + data + ", notification: " + notification);
+        Intent intent = new Intent(ACTION_REMOTE_MESSAGE);
+        intent.putExtra(EXTRA_REMOTE_MESSAGE, remoteMessage);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
 }
